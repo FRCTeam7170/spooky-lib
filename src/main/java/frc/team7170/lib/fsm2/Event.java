327f8157e702af38ecf0a9030b29fdf6e9799cf7
robert <robertrussell.72001@gmail.com>
2019-10-21 12:03:25 -0700

    Add documentation for Event class

--- src/main/java/frc/team7170/lib/fsm2/Event.java
+++ src/main/java/frc/team7170/lib/fsm2/Event.java
@@ -2,13 +2,50 @@ package frc.team7170.lib.fsm2;
 
 import java.util.Map;
 
+/**
+ * {@code Event} represents the immutable context for a state change in a {@link FiniteStateMachine FiniteStateMachine}.
+ *
+ * @author Robert Russell
+ */
 public final class Event {
 
+    /**
+     * The {@link FiniteStateMachine FiniteStatemachine} in which the state change occurred.
+     */
     public final FiniteStateMachine machine;
+
+    /**
+     * The src (source) {@linkplain State state} (i.e. the state being transitioned from). The src and dst (destination)
+     * states are equal for internal transitions and reflexive transitions.
+     */
     public final State src;
+
+    /**
+     * The dst (destination) {@linkplain State state} (i.e. the state being transitioned to). The dst and src (source)
+     * states are equal for internal transitions and reflexive transitions.
+     */
     public final State dst;
+
+    /**
+     * The {@linkplain Transition transition} that is occurring. If the state change was forced (i.e. via one of the
+     * {@code forceTo} methods on a {@link FiniteStateMachine FiniteStateMachine}), then this is {@code null}.
+     */
     public final Transition transition;
+
+    /**
+     * The trigger that caused the state change. If the state change was forced (i.e. via one of the {@code forceTo}
+     * methods on a {@link FiniteStateMachine FiniteStateMachine}), then this is {@code null}.
+     */
     public final String trigger;
+
+    /**
+     * The arguments passed to the call of {@code trigger} or {@code forceTo} (on a
+     * {@link FiniteStateMachine FiniteStateMachine}) that caused the state change.
+     * This map is guaranteed to be non-null and, unless {@code trigger}/{@code forceTo} was invoked with an immutable
+     * map, mutable.
+     * Moreover, since the same {@code Event} object is passed to every callback in the callback chain for a state
+     * change, changes to this map in one callback will be visible to other callbacks.
+     */
     public final Map<String, Object> args;
 
     Event(FiniteStateMachine machine,
