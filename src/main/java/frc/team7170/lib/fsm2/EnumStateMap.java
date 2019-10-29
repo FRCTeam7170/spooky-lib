a7cb781b8fac7b9129a01d79721f49be401cef8d
robert <robertrussell.72001@gmail.com>
2019-10-29 12:36:27 -0700

    Add documentation for EnumStateMap.java

--- src/main/java/frc/team7170/lib/fsm2/EnumStateMap.java
+++ src/main/java/frc/team7170/lib/fsm2/EnumStateMap.java
@@ -4,15 +4,30 @@ import java.util.EnumMap;
 import java.util.Map;
 import java.util.function.Supplier;
 
+
+/**
+ * An implementation of {@link StateMap StateMap} for states of an enum type.
+ *
+ * @param <S> the state type.
+ * @param <T> the trigger type.
+ *
+ * @author Robert Russell
+ */
 class EnumStateMap<S extends Enum<S> & State, T> implements StateMap<S, T> {
 
     private final Map<S, StateBundle<T>> bundleMap;
 
+    /**
+     * @param stateEnum the class object of the enum whose constants are to be used for states.
+     * @param mapSupplier a supplier returning maps appropriate for the trigger type.
+     * @throws IllegalArgumentException if the given enum has no constants.
+     */
     EnumStateMap(Class<S> stateEnum, Supplier<Map<T, Transition<T>>> mapSupplier) {
         S[] enumConstants = stateEnum.getEnumConstants();
         if (enumConstants.length == 0) {
             throw new IllegalArgumentException("state machines must have at least one state");
         }
+        // Use EnumMap because it is fast.
         bundleMap = new EnumMap<>(stateEnum);
         for (S s : enumConstants) {
             bundleMap.put(s, new StateBundle<>(s, mapSupplier));
